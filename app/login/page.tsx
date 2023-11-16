6cb31d193743c79bea4cf96be0cc5765c96c795c
Vivek <vivek7405@gmail.com>
2023-11-17 03:12:19 +0530

    Used Metronics Logo in the sign in page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Form from "@/components/form";
 import Link from "next/link";
+import { MLogoSVG } from "@/lib/assets/m-logo";
 
 export default function Login() {
   return (
@@ -8,14 +9,15 @@ export default function Login() {
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
           <Link href="/">
-            <Image
+            <MLogoSVG />
+            {/* <Image
               src="/logo.png"
               priority
               alt="Logo"
               className="h-10 w-10 rounded-full"
               width={20}
               height={20}
-            />
+            /> */}
           </Link>
           <h3 className="text-xl font-semibold">Sign In</h3>
           <p className="text-sm text-gray-500">
